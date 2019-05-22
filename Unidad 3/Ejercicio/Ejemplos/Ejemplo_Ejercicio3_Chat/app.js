a22b5451dc5ded1ea58c6a22af44dc15b0714909
Luis Morales-Navarro <lmn297@nyu.edu>
2019-05-22 09:52:54 -0600

    ejercicio u3

---
+++ Unidad 3/Ejercicio/Ejemplos/Ejemplo_Ejercicio3_Chat/app.js
@@ -0,0 +1,45 @@
+//Requerimientos 
+var express = require("express");
+var logger = require('morgan');
+var ultimoMensaje = "Hola, ¿todo bien?";
+var bodyParser = require('body-parser');
+
+//Crear objeto 'express' 
+var app = express();
+
+//escribe en la terminal las solicitudes que recibe
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+//Crea un directorio de vistas
+app.set("views", __dirname);
+//Asigna el lenguaje del motor y la extensión .html
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+//Agrega una conexión al folder público 
+app.use(express.static(__dirname + '/publico'));
+
+//Rutas
+app.get("/", function(solicitud, respuesta){
+	respuesta.render('indice');
+});
+
+app.get("/ultimoMSJ", function(solicitud,respuesta){
+	var data={"mensaje": ultimoMensaje};
+	respuesta.json(data);
+});
+
+app.post("/nuevoMSJ", function(solicitud,respuesta){
+	ultimoMensaje=solicitud.body.mensaje;
+	var data = {"recibido": true};
+	respuesta.json(data);
+});
+
+
+app.get("*", function(solicitud, respuesta){
+	respuesta.send('No hay nada aquí');
+});
+
+// Iniciar el servidor
+app.listen(3000);
+console.log('Express inició en el puerto 3000');
---
+++ Unidad 3/Ejercicio/Ejemplos/Ejemplo_Ejercicio3_Chat/publico/boceto.js
@@ -0,0 +1,126 @@
+var mensajes = [];
+var palabra = "";
+var ultimoMensaje = "nada";
+var recibido = false;
+
+function setup() {
+  createCanvas(400, 400);
+  primerMSJ();
+}
+
+function draw() {
+  background(220);
+  stroke(50);
+  fill(0);
+  rect(100,10,200,370,20);
+  fill(250);
+  rect(110,20,180,350,20);
+  fill(250);
+  rect(120,330,140,30,20);
+  noStroke();
+  fill(0,255,0);
+  ellipse(275,345,20,20);
+  fill(255);
+  triangle(270,340,270,350,280,345);
+  for (var i = 0; i < mensajes.length; i++) {
+    mensajes[i].mostrar(mensajes.length-i);
+  }
+  fill(0);
+  text(palabra,130,350);
+  if (recibido==true){
+    push();
+    translate(270,305);
+    rotate(radians(45));
+    fill(255);
+    rect(0,0,5,13);
+    rect(-5,12,10,5);
+    pop();
+
+  }
+}
+
+
+function keyPressed(){
+  if(key!="Backspace" && key!="Enter"){
+    //agrega letra
+    palabra = palabra + key;
+  }else if (key=="Backspace"){
+    //elimina última letra
+    palabra=palabra.slice(0,-1);
+  }else if(key=="Enter"){
+    nuevoMensaje(palabra);
+    palabra = "";
+  }
+}
+
+function mousePressed(){
+  var distancia = dist(mouseX, mouseY, 275,345);
+
+  if (distancia < 10 && palabra!="") {
+    nuevoMensaje(palabra);
+    palabra = "";
+  }
+}
+
+function nuevoMensaje(texto){
+  recibido = false
+  nuevoMSJ(texto);
+  mensajes.push(new Burbuja(texto,"enviado"));
+}
+
+function Burbuja (tempText, tempTipo){
+  this.text = tempText;
+  this.tipo = tempTipo;
+  this.mostrar = function(num,total){
+    if (num < 9){
+      num = 9-num;
+    if (this.tipo=="recibido"){
+        fill(150);
+        rect(120,15+num*35,140,30,20);
+        fill(0);
+        text(this.text,130,35+num*35);
+      }else{
+        fill(0,255,0);
+        rect(140,15+num*35,140,30,20);
+        fill(0);
+        text(this.text,160,35+num*35);
+      }
+    } 
+  }
+}
+
+function primerMSJ(){
+  $.ajax({
+    url: '/ultimoMSJ',
+    type: 'GET',
+    dataType: 'json',
+    error: function(respuesta){
+      console.log(respuesta);
+      alert("¡Oh No! Intente de nuevo");
+    },
+    success: function(respuesta){
+      console.log("WooHoo!");
+      ultimoMensaje = respuesta.mensaje;
+      mensajes[0] = new Burbuja(ultimoMensaje,"recibido");
+    }
+  });
+}
+
+function nuevoMSJ(MSJ){
+  var enviar = {"mensaje":MSJ};
+  console.log(enviar);
+  $.ajax({
+    url: '/nuevoMSJ',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(enviar),
+    error: function(respuesta){
+      console.log(respuesta);
+      alert("¡Oh No! Intente de nuevo");
+    },
+    success: function(respuesta){
+      console.log("WooHoo!");
+      recibido = true;
+    }
+  });
+}
